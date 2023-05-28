665f9b3671a0d8f4647dcb0b5814b91bd20a7f80
Marcos Daniel Mercado <marcosmerk156@gmail.com>
2023-05-28 18:00:43 -0300

    feat: improve responsive at homePage

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,26 +1,49 @@
 import React from "react";
 import bgHome from "@Assets/backgroundObs.jpg";
-import TitlePage from "@Components/TitlePage";
+import { Link } from "react-router-dom";
+import headerBanner from "@Assets/mainPicture.png";
+import Button from "@Components/Button";
 
 const Home = () => {
   return (
-    <div className="h-outlet w-full ">
-      <TitlePage title="Observatorio de fallos de consumo" />
-      <div className="w-full h-1/2 py-4 px-2 md:px-10  flex justify-center gap-x-8 ">
+    <div className="h-outlet w-full">
+      <div
+        style={{
+          height: "100%",
+          backgroundImage: `url(${headerBanner})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+        }}
+        className="flex flex-col items-end justify-end"
+      >
+        <div className="bg-navbar p-4 mb-10" style={{ width: "80%" }}>
+          <span className="text-white text-4xl font-bold">
+            OBSERVATORIO DE FALLOS DE CONSUMO
+          </span>
+        </div>
+      </div>
+      <div className="w-full sm:h-1/2 py-4 px-2 md:px-10 flex justify-center gap-x-8 ">
         <img
           src={bgHome}
-          alt="imagen representativa"
+          alt="imagen ¿que es el observatorio?"
           className="hidden xl:block"
         />
         <div className="w-full md:w-1/2 flex flex-col gap-y-5 justify-center items-center">
-          <h1 className="text-subtitle text-4xl text-center whitespace-nowrap">
+          <h1 className="text-subtitle text-3xl sm:text-4xl text-justify whitespace-nowrap">
             ¿Qué es el observatorio?
           </h1>
-          <span className="text-black">
+          <span className="text-black text-base sm:text-xl">
             Es un proyecto comuntario organizado por Usuarios y Consumidores
             Unidos cuyo objetivo es contener, compilar la mayor cantidad de
             antecedentes jurisprudenciales en materia de defensa del consumidor.
           </span>
+        </div>
+      </div>
+      <div>
+        <div className="w-1/2 mx-auto">
+          <Link to={"/buscarFallo"}>
+            <Button children={"Buscar Fallos"} />
+          </Link>
         </div>
       </div>
     </div>
