70aa550f2b61d5f3fca155e7e698e60c67e3ca2f
Marcos Daniel Mercado <marcosmerk156@gmail.com>
2023-10-27 17:37:47 -0300

    feat: improve usage at form( more texarea length) and navBar

--- src/components/Layout/NavBar/index.jsx
+++ src/components/Layout/NavBar/index.jsx
@@ -10,14 +10,17 @@ const NavBar = () => {
     { name: "Buscador", path: "/buscador", id: 4 },
   ];
   const [open, setOpen] = useState(false);
+
+  const closeMenu = () => {
+    setOpen(false);
+  };
   return (
     <div className="shadow-md w-full bg-navbar">
       <div className="flex items-center justify-between py-4 md:px-10 px-7 h-20">
         <div>
           <Link
             to="/"
-            className="flex items-center gap-x-2.5 divide-x divide-x-white"
-          >
+            className="flex items-center gap-x-2.5 divide-x divide-x-white">
             <img src={balanceIcon} className="h-6" />
             <p className="pl-2 text-white">OBSERVATORIO</p>
           </Link>
@@ -25,22 +28,21 @@ const NavBar = () => {
 
         <div
           onClick={() => setOpen(!open)}
-          className="text-3xl right-8 top-6 cursor-pointer md:hidden text-white"
-        >
+          className="text-3xl right-8 top-6 cursor-pointer md:hidden text-white">
           {!open ? <MdMenu /> : <MdMenuOpen />}
         </div>
 
         <ul
           className={`text-white  md:flex md:items-center md:pb-0  md:static  md:z-auto md:w-auto md:pl-0 left-0 w-full pl-9 absolute transition-all duration-500 ease-in ${
             open ? "top-14 bg-navbar " : "top-[-490px]"
-          }`}
-        >
+          }`}>
           {Links.map((link) => (
             <li
               key={link.id}
-              className="md:ml-8 text-xl md:my-0 my-7 hover:border-b-2"
-            >
-              <Link to={link.path}>{link.name}</Link>
+              className="md:ml-8 text-xl md:my-0 my-7 hover:border-b-2">
+              <Link to={link.path} onClick={closeMenu}>
+                {link.name}
+              </Link>
             </li>
           ))}
         </ul>
