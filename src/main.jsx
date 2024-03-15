01f96e451fcc66c6b7aa26556d52c3ca70a24543
Marcos Daniel Mercado <marcosmerk156@gmail.com>
2024-03-15 04:58:54 -0300

    feat: delete strict mode from Main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,7 @@ import "./styles/index.css";
 import { VerdictsContextProvider } from "./context/VerdictsContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <VerdictsContextProvider>
-      <App />
-    </VerdictsContextProvider>
-  </React.StrictMode>
+  <VerdictsContextProvider>
+    <App />
+  </VerdictsContextProvider>
 );
