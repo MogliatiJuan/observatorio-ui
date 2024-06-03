b0d0caaee2006ffb418113b3765d923a858f4ca9
Juan Manuel Mogliati <mogliatijm@gmail.com>
2024-06-03 08:03:51 -0300

    fix: :ambulance: fix imports

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./routes/App";
 import "./styles/index.css";
 import { VerdictsContextProvider } from "./context/VerdictsContext";
-import { DataProvider } from "./context/selectsContext";
+import { DataProvider } from "./context/SelectsContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <VerdictsContextProvider>
